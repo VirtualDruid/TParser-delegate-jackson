2f8f287ab7bf30159f3a604f6f3c8aea2bfbcca0
VirtaulDruid <VirtualDruid70@gmail.com>
2020-02-08 19:56:49 +0800

    update package namespace

--- src/main/java/tparser/delegate/JacksonDelegate.java
+++ src/main/java/tparser/delegate/JacksonDelegate.java
@@ -1,3 +1,5 @@
+package tparser.delegate;
+
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
